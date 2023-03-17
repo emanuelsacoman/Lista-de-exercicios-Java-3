8c760d52d13887e42398d36b97df7127d7130efe
Emanuel Vinícius Sacoman <emanuelsacoman@gmail.com>
2023-03-17 00:49:40 -0300

    Envio da atv 3 (txt)

---
+++ atv3/src/X.java
@@ -0,0 +1,6 @@
+public class X {
+    private int cont; // <- public resolveria o problema.
+    public void setCont(int cont) {
+        this.cont = cont;
+    }
+}
---
+++ atv3/src/Y.java
@@ -0,0 +1,6 @@
+public class Y {
+    public static void main (String[ ] args) {
+        X ob = new X();
+        ob.cont = 10;
+    }
+} 
